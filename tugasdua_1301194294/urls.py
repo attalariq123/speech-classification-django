91abdeceb30d6f3339cfb6b6c8ead60e878f3a2f
attalariq123 <attalariq002@gmail.com>
2022-07-26 13:40:18 +0700

    fix route

--- tugasdua_1301194294/urls.py
+++ tugasdua_1301194294/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'tugasdua'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('course/', views.course, name='course')
