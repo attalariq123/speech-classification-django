a423bd8abc5ad0ef6a17b68f5ded61237456d4f7
attalariq123 <attalariq002@gmail.com>
2022-08-03 10:37:19 +0700

    model adjustment

--- email_network/admin.py
+++ email_network/admin.py
@@ -1,4 +1,7 @@
+from ast import Div
 from django.contrib import admin
-from .models import EmailNetwork
+from .models import EmailNetwork, Division, Department
 
 admin.site.register(EmailNetwork)
+admin.site.register(Division)
+admin.site.register(Department)
--- email_network/views.py
+++ email_network/views.py
@@ -1,9 +1,10 @@
 import email
 from django.shortcuts import render
 
-from email_network.models import EmailNetwork
+from email_network.models import Division, EmailNetwork
 
 def index(request):
     email_network = EmailNetwork.objects.all()
-    context = {'email_network': email_network}
+    division = Division.objects.all()
+    context = {'email_network': email_network, 'division': division}
     return render(request, 'email_network/index.html', context)
